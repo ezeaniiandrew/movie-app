cbace6d81443c39f32f2238992493d5488aa7ed2
Andrew Ezeani <ezeaniiandrew@gmail.com>
2023-09-11 08:52:38 +0100

    configured absolute import

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
+
+const folders = fs.readdirSync("./src", { withFileTypes: true });
+const fileNames = folders
+  .filter((dirent) => dirent.isDirectory())
+  .map((dirent) => dirent.name);
+
+const filePaths = fileNames.reduce(
+  (acc, curr) => ({
+    ...acc,
+    [curr]: `@/${curr === "src" ? curr : "src/" + curr}`,
+  }),
+  {}
+);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      ...filePaths,
+    },
+  },
+});
